05ae76af207c877f5c6e151145686b7435e32f59
martin legrand <martin.legrand@epitech.eu>
2025-01-03 17:34:32 +0100

    CI : yml mkdir fix

--- data/data_harverser/data_pipeline.sh
+++ data/data_harverser/data_pipeline.sh
@@ -2,6 +2,11 @@
 
 echo "Running data pipeline for < $1 >"
 
+mkdir -p data/images_training
+mkdir -p data/prepared_data/csv
+mkdir -p data/prepared_data/images
+mkdir -p data/prepared_data/sounds
+touch data/prepared_data/csv/$1.csv
 python3 data/data_harverser/main.py --scrawl $1 --config data/data_harverser/config.json
 python3 data/data_harverser/main.py --download $1 --config data/data_harverser/config.json
 python3 data/data_harverser/main.py --sound2spec $1 --config data/data_harverser/config.json
