1f441cbff528606624de70b71a02c021941e8749
martin legrand <martin.legrand@epitech.eu>
2025-01-03 18:06:32 +0100

    fixing CI path again

--- data/data_harverser/data_pipeline.sh
+++ data/data_harverser/data_pipeline.sh
@@ -2,6 +2,9 @@
 
 echo "Running data pipeline for < $1 >"
 
+
+mkdir -p data/prepared_data/csv
+touch data/prepared_data/csv/$1.csv
 python3 data/data_harverser/main.py --scrawl $1 --config data/data_harverser/config.json
 python3 data/data_harverser/main.py --download $1 --config data/data_harverser/config.json
 python3 data/data_harverser/main.py --sound2spec $1 --config data/data_harverser/config.json
