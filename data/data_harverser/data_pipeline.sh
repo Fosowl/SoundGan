e4790f1435f6eca485375c8e0c0d1d17bbd3b22c
martin legrand <martin.legrand@epitech.eu>
2025-01-04 11:54:56 +0100

    CI: fix

--- data/data_harverser/data_pipeline.sh
+++ data/data_harverser/data_pipeline.sh
@@ -6,6 +6,6 @@ echo "Running data pipeline for < $1 >"
 mkdir -p data/prepared_data/csv
 touch data/prepared_data/csv/$1.csv
 cd data/data_harverser
-python3 ./main.py --scrawl $1 --config data/data_harverser/config.json
-python3 ./main.py --download $1 --config data/data_harverser/config.json
-python3 ./main.py --sound2spec $1 --config data/data_harverser/config.json
+python3 ./main.py --scrawl $1 --config config.json
+python3 ./main.py --download $1 --config config.json
+python3 ./main.py --sound2spec $1 --config config.json
